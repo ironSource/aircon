e99f2dd50236ae02ecd4fe30dee1a4e7f52eb79c
Hanan <ntnel6@gmail.com>
2019-06-22 00:56:27 +0300

    Added quickfix for missing source annotation lint error

--- aircon-lint/src/main/java/com/ironsource/aura/aircon/lint/detector/configField/MissingSourceDetector.java
+++ aircon-lint/src/main/java/com/ironsource/aura/aircon/lint/detector/configField/MissingSourceDetector.java
@@ -2,8 +2,11 @@ package com.ironsource.aura.aircon.lint.detector.configField;
 
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.LintFix;
+import com.ironsource.aura.aircon.common.annotations.Source;
 import com.ironsource.aura.aircon.lint.utils.ConfigElementsUtils;
 
+import org.jetbrains.uast.UElement;
 import org.jetbrains.uast.UField;
 
 /**
@@ -21,7 +24,19 @@ public class MissingSourceDetector
 	@Override
 	protected void visitConfigField(final UField node) {
 		if (!ConfigElementsUtils.hasSourceAnnotation(node) && !ConfigElementsUtils.hasSourceAnnotation(node.getContainingClass())) {
-			report(node);
+			report(node, createFix(node));
 		}
+	}
+
+	private LintFix createFix(final UField node) {
+		final LintFix.GroupBuilder groupBuilder = LintFix.create()
+		                                                 .group();
+		groupBuilder.add(addSourceAnnotationFix(node, "field"));
+		groupBuilder.add(addSourceAnnotationFix(node.getUastParent(), "feature interface"));
+		return groupBuilder.build();
+	}
+
+	private LintFix addSourceAnnotationFix(UElement target, String targetName) {
+		return addAnnotationFix(target, targetName, Source.class, "SomeConfigSource.class");
 	}
 }

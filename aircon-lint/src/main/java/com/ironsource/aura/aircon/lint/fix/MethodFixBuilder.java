c536e7936c8b0249b7223dec46c989e3a124661e
Hanan <ntnel6@gmail.com>
2019-06-22 16:55:04 +0300

    Added quickfix for wrong config aux method signature lint error

--- aircon-lint/src/main/java/com/ironsource/aura/aircon/lint/fix/MethodFixBuilder.java
+++ aircon-lint/src/main/java/com/ironsource/aura/aircon/lint/fix/MethodFixBuilder.java
@@ -1,6 +1,8 @@
 package com.ironsource.aura.aircon.lint.fix;
 
 import com.android.tools.lint.detector.api.JavaContext;
+import com.intellij.lang.jvm.JvmModifier;
+import com.ironsource.aura.aircon.lint.utils.ElementUtils;
 
 import org.jetbrains.uast.UMethod;
 
@@ -10,7 +12,8 @@ import org.jetbrains.uast.UMethod;
 public class MethodFixBuilder
 		extends IssueFixBuilder<UMethod> {
 
-	private static final String METHOD_NAME_REGEX = "[a-zA-Z][a-zA-Z0-9]+\\(";
+	private static final String NAME_REGEX        = "[a-zA-Z][a-zA-Z0-9]";
+	private static final String METHOD_NAME_REGEX = NAME_REGEX + "+\\(";
 
 	public MethodFixBuilder(JavaContext context, UMethod target, String name) {
 		super(context, target, name);
@@ -21,5 +24,23 @@ public class MethodFixBuilder
 		                                        .getPresentableText();
 		mGroupBuilder.add(replacePattern(String.format(".*(%s) " + METHOD_NAME_REGEX, currentReturnType), returnType));
 		return this;
+	}
+
+	public MethodFixBuilder setVisibility(JvmModifier visibility) {
+		final JvmModifier visibilityModifier = ElementUtils.getVisibilityModifier(mTarget);
+		mGroupBuilder.add(replacePattern(String.format(".*(%s).*", getModifierName(visibilityModifier)), getModifierName(visibility)));
+		return this;
+	}
+
+	public MethodFixBuilder addModifier(JvmModifier modifier) {
+		final String currentReturnType = mTarget.getReturnType()
+		                                        .getPresentableText();
+		mGroupBuilder.add(replacePattern(String.format(".*()%s " + METHOD_NAME_REGEX, currentReturnType), getModifierName(modifier) + " "));
+		return this;
+	}
+
+	private String getModifierName(final JvmModifier visibility) {
+		return visibility == JvmModifier.PACKAGE_LOCAL ? "" : visibility.name()
+		                                                                .toLowerCase();
 	}
 }
